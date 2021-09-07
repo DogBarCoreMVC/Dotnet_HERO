c37b0266a39d0cc0159a9819bb3033f49982c79a
GitDogBar <Dogbar>
2021-09-07 14:40:28 +0700

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Dotnet_HERO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] //https://locahost:5001/products (dev)
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult GetProducts()
+        {
+            var Products = new List<string>();
+            Products.Add("Nokia");
+            Products.Add("Oppo");
+            return Ok(Products);//Status Success 200
+        }
+
+        [HttpGet("{id}")] //https://locahost:5001/products/{id}
+        
+        public IActionResult GetProductsById(int id)
+        {
+            var Products = new List
+            return null;
+        }
+    }
+}
